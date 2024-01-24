095f2dc15abd580beceee02021d8442af6386b8b
Nurnabi200 <151782668+Nurnabi200@users.noreply.github.com>
2024-01-24 14:39:26 +0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,90 @@
+clear
+
+echo " 
+
+       𝑾𝒆𝒍𝒄𝒐𝒎𝒆 𝒎𝒚 𝒕𝒐𝒐𝒍𝒔 
+
+  _   _ _   _ ____  _   _    _    ____ ___  
+ | \ | | | | |  _ \| \ | |  / \  | __ )_ _| 
+ |  \| | | | | |_) |  \| | / _ \ |  _ \| |  
+ | |\  | |_| |  _ <| |\  |/ ___ \| |_) | |  
+ |_| \_|\___/|_| \_\_| \_/_/   \_\____/___| 
+                                            
+                                            
+"
+echo " ==============================================================="
+
+echo "   Tools Information "
+echo "
+"
+echo "    Tools Name :- TERMUX BASIC SETUP "
+echo "    AUTHOR NAME : MD NURNABI ISLAM"
+echo "    GitHub: Nurnabi200 "
+echo "    phone: ******65655"
+echo "    Facebook I'd name: Md Nurnabi Islam "
+echo "    
+"
+echo " =========================================================="
+echo "
+"
+apt update
+apt upgrade
+apt update
+apt upgrade
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pkg install nano -y
+pip2 install requests
+ pip2 install mechanize
+ pip2 install tqdm
+ pip2 install bs4
+pip2 install requests
+pip install future
+apt install ruby
+apt install openssh
+apt install wget
+apt install curl
+pkg install python
+pkg install python2
+pkg install bash
+pkg install php
+pkg install php 
+pkg update -y
+pip install mechanize lolcat
+pip install requests bs4
+
+pip2 install bs4
+
+pip install futures
+
+pip2 install futures
+
+pkg install wget
+
+pkg install php
+
+pkg install pip2
+
+pip install rich
+
+pip install cython
+
+pkg list-all
+
+pkg install sl
+
+pkg install bash
+
+pkg install openssh
+
+pkg install curl php
+
+termux-setup-storage
+
+echo "       𝐒𝐞𝐭𝐮𝐩 𝐟𝐢𝐧𝐢𝐬𝐡𝐞𝐝   "
+clear
+echo "
+        𝐓𝐡𝐚𝐧𝐤𝐬 𝐲𝐨𝐮 𝐟𝐨𝐫 𝐮𝐬𝐢𝐧𝐠 𝐌𝐲 𝐭𝐨𝐨𝐥𝐬
+          
+"
